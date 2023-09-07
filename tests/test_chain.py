6056847143f682122d532970d5be3ecc4390dd1f
Matt Florence <matt@mattflo.com>
2023-09-06 22:25:13 -0600

    introduce simple integration smoke test

---
+++ tests/test_chain.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import pytest
+import time
+from weather_chat_ai.chat import WeatherChat
+
+
+def dated_tag():
+    current_date = datetime.now().strftime("%Y%m%d")
+    epoch_timestamp = str(time.time())[-4:]
+    return f"{current_date}-{epoch_timestamp}"
+
+
+@pytest.mark.asyncio
+async def test_chain():
+    day_of_week = datetime.now().strftime("%A")
+    chain = WeatherChat.create_chain(tags=[dated_tag()])
+    queries = [
+        "what is the weather in london today?",
+        f"what is the weather next {day_of_week}?",
+        "What is the weather today in Denver, CO?",
+    ]
+
+    results = []
+
+    for query in queries:
+        result = await chain.acall({"input": query})
+        results.append(result)
+
+    print(results)
